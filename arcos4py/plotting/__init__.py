5b0b21b0c453feb02b0244e8cc942b032ebecc53
Benjamin Graedel <benjamin.graedel@unibe.ch>
2023-08-10 14:17:21 +0200

    Bump version: 0.2.0 → 0.2.1

--- arcos4py/plotting/__init__.py
+++ arcos4py/plotting/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Benjamin Graedel"""
 __email__ = "benjamin.graedel@unibe.ch"
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from ._plotting import NoodlePlot, dataPlots, plotOriginalDetrended, statsPlots
 
