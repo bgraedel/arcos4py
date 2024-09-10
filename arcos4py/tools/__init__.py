87baa474219ca5cd5ac70af9e5a79b398827b957
Benjamin Graedel <benjamin.graedel@unibe.ch>
2024-09-10 17:26:37 +0200

    Bump version: 0.2.4 → 0.2.5

--- arcos4py/tools/__init__.py
+++ arcos4py/tools/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Benjamin Graedel"""
 __email__ = "benjamin.graedel@unibe.ch"
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 
 from ._binarize_detrend import binData
 from ._cleandata import clipMeas, interpolation, remove_image_background
