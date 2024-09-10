b1634646338d13d186cbd204289b78f4a53aa2e8
Benjamin Graedel <benjamin.graedel@unibe.ch>
2024-09-10 17:34:13 +0200

    revert version change

--- arcos4py/plotting/__init__.py
+++ arcos4py/plotting/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Benjamin Graedel"""
 __email__ = "benjamin.graedel@unibe.ch"
-__version__ = '0.2.6'
+__version__ = '0.2.4'
 
 from ._plotting import NoodlePlot, dataPlots, plotOriginalDetrended, statsPlots
 
